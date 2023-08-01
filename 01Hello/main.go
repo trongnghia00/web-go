d5ceeb2656eda745bdfa70a8b3ef43985eba2a10
Trong Nghia <trongnghia77@live.com>
2023-08-01 23:50:00 +0700

    4-var-func

--- 01Hello/main.go
+++ 01Hello/main.go
@@ -4,4 +4,21 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+
+	var newLine string
+	var i int
+
+	newLine = "Goodbye world !"
+	i = 10
+
+	fmt.Println(newLine)
+	fmt.Println("i is set to", i)
+
+	someThing, someThingElse := saySomething()
+
+	fmt.Println("The func returned", someThing, someThingElse)
+}
+
+func saySomething() (string, string) {
+	return "something", "else"
 }
