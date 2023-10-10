caed4a1f0bd5783aa7b681c1791672ef4651485b
Trong Nghia <trongnghia77@live.com>
2023-10-10 23:29:05 +0700

    17-web-handle-func

--- 02webbasic/16webhelloworld/main.go
+++ 02webbasic/16webhelloworld/main.go
@@ -5,10 +5,23 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello Word !!")
-	})
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Word !!")
+}
 
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 3)
+	fmt.Fprintf(w, "This is About page. 2 + 3 = %d", sum)
+}
+
+func addValues(x, y int) int {
+	return x + y
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println("Starting application at port 8080 .....")
 	http.ListenAndServe(":8080", nil)
 }
