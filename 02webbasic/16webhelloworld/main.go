bbdc4c3d4c3b0072e09dae43b5472298fab09662
Trong Nghia <trongnghia77@live.com>
2024-02-22 10:23:35 +0700

    19-web-template

--- 02webbasic/16webhelloworld/main.go
+++ 02webbasic/16webhelloworld/main.go
@@ -1,46 +1,54 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello Word !!")
+	renderTemplates(w, "home.page.tmpl")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 3)
-	fmt.Fprintf(w, "This is About page. 2 + 3 = %d", sum)
+	renderTemplates(w, "about.page.tmpl")
 }
 
-func Divide(w http.ResponseWriter, r *http.Request) {
-	f, err := devideValues(100.0, 0.0)
+func renderTemplates(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Fprintf(w, "Cannot divide by zero")
+		fmt.Println("error parsing template: ", err)
 		return
 	}
-	fmt.Fprintf(w, "%f divided by %f is %f", 100.0, 0.0, f)
 }
 
-func addValues(x, y int) int {
-	return x + y
-}
+// func Divide(w http.ResponseWriter, r *http.Request) {
+// 	f, err := devideValues(100.0, 0.0)
+// 	if err != nil {
+// 		fmt.Fprintf(w, "Cannot divide by zero")
+// 		return
+// 	}
+// 	fmt.Fprintf(w, "%f divided by %f is %f", 100.0, 0.0, f)
+// }
 
-func devideValues(x, y float32) (float32, error) {
-	if y == 0 {
-		err := errors.New("cannot divide by zero")
-		return 0, err
-	}
-	result := x / y
-	return result, nil
-}
+// func addValues(x, y int) int {
+// 	return x + y
+// }
+
+// func devideValues(x, y float32) (float32, error) {
+// 	if y == 0 {
+// 		err := errors.New("cannot divide by zero")
+// 		return 0, err
+// 	}
+// 	result := x / y
+// 	return result, nil
+// }
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
+	// http.HandleFunc("/divide", Divide)
 
 	fmt.Println("Starting application at port 8080 .....")
 	http.ListenAndServe(":8080", nil)
